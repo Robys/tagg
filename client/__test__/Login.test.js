5f6851cf87de73f2c2c1a205167b0ad2ce5e48c8
Robert Oliveira <robert.oliveira.f@gmail.com>
2021-03-31 12:08:09 -0300

    ADD LOGIN UNIT TEST

--- client/__test__/Login.test.js
+++ client/__test__/Login.test.js
@@ -4,14 +4,15 @@ import {waitFor,render,fireEvent} from '@testing-library/react'
 import {MockedProvider} from '@apollo/react-testing'
 import '@testing-library/jest-dom/extend-expect';
 import {LOGIN} from '../src/api/mutations'
-import Login from '../src/views/Login'
+import {useMutation} from "@apollo/react-hooks"
+
+const info = {email:"william@emil.com",password:"456456"}
 
 const mock = [{
     request:{
         query:LOGIN,
-        variables:{email:"william@emil.com",password:"456456"}
+        variables:{email:info.email,password:info.password}
     },
-    error: new Error('Something happend'),
     result : jest.fn(()=>({
         data: {
             login: {
@@ -24,29 +25,61 @@ const mock = [{
     }))
 }]
 
+const LoginComp = ({email,password}) =>{
+  const [login,{data,loading,error}] = useMutation(LOGIN,{variables:{email,password}})
+  return (
+    <div>
+    <button data-testid="login"  onClick={e => login()}>
+      Login
+    </button>
 
-describe('testing login mutation', ()=>{
+    {loading? <p>Loading ...</p> :""}
+    {error? <p>error {JSON.stringify(error)}</p> : ""}
+    {data? <p data-testid="ndata">{JSON.stringify(data)}</p> : ""}
+    </div>
+  )
+}
+
+    test('mutation should call with the button pressed',async ()=>{
+      const container = render(
+        <MockedProvider mocks={mock} addTypename={false}>
+          <LoginComp email={info.email} password={info.password}/>
+        </MockedProvider>)
+          await waitFor(() => new Promise((res) => setTimeout(res, 0)));
+          const btn = await container.findByTestId('login')
+          fireEvent.click(btn)
+          await waitFor(() => new Promise((res) => setTimeout(res, 0)));
+         expect(container).toMatchSnapshot();
+    })
+
+
+
+/**
+ * 
+ * describe('testing login mutation', ()=>{
     
     test('mutation should call with the button pressed',async ()=>{
        
 
            const container = render(
                 <MockedProvider mocks={mock} addTypename={false}>
-                   <Login />
+                  <Mutation mutation={LOGIN} variables={{email:"william@emil.com",password:"456456"}}>
+                    {(data,loading,error) => {
+                      if(loading) return <p>Loading ...</p>
+                      if(error) return <p>Err.. {error.message}</p>
+                      if(data) return data
+                    }}
+
+                  </Mutation>
+                  
                 </MockedProvider>
             )
 
-            fireEvent.click(container.getByTestId('login'))
-
-            await waitFor(() => new Promise((res) => setTimeout(res, 0)));
-           
-             expect(container).toMatchSnapshot();
+         expect(container).toMatchSnapshot();
     })
 
 
 })
-
-
-
+ */
 
 
