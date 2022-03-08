10919e8cc5a5537377d739b88f15e2ed786e0f3e
Robert Oliveira <robert.oliveira.f@gmail.com>
2022-03-07 22:26:49 -0300

    facebook auth scope update

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ const server = new ApolloServer({
     playground:true,
 })
 
-app.get('/auth/facebook',passport.authenticate('facebook',{ scope: 'public_profile' }))
+app.get('/auth/facebook',passport.authenticate('facebook',{ scope: ['email', 'public_profile','user_location'] }))
 app.get('/auth/facebook/callback',passport.authenticate('facebook',{
     successRedirect: process.env.SUCCESS_URL,
     failureRedirect: process.env.FRONT_END_URL
