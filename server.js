6aac67dc7f7d2ba2cc44b5943aeebb38fe601c22
Robert Oliveira <robert.oliveira.f@gmail.com>
2022-03-08 15:11:10 -0300

    sadfasdfasdfasdf

--- server.js
+++ server.js
@@ -96,11 +96,12 @@ const server = new ApolloServer({
     playground:true,
 })
 
-app.get('/auth/facebook',passport.authenticate('facebook',{ scope: ['email', 'public_profile','user_location'] }))
+app.get('/auth/facebook',passport.authenticate('facebook'))
 app.get('/auth/facebook/callback',passport.authenticate('facebook',{
     successRedirect: process.env.SUCCESS_URL,
     failureRedirect: process.env.FRONT_END_URL
 }))
+
 
 server.applyMiddleware({app,cors: true})
 
